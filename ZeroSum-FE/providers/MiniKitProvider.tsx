6dafd325cf6d21424446cbb61024c12063e75233
ogazboiz <ogazboizakpolo@gmail.com>
2025-11-18 06:24:53 +0100

    Change default network to Celo Sepolia testnet (chainId: 11142220)

--- ZeroSum-FE/providers/MiniKitProvider.tsx
+++ ZeroSum-FE/providers/MiniKitProvider.tsx
@@ -1,13 +1,13 @@
 'use client';
 import { MiniKitProvider } from '@coinbase/onchainkit/minikit';
 import { ReactNode } from 'react';
-import { baseSepolia } from 'wagmi/chains';
+import { celoAlfajores } from 'wagmi/chains';
 
 export function MiniKitContextProvider({ children }: { children: ReactNode }) {
   return (
     <MiniKitProvider 
       apiKey={process.env.NEXT_PUBLIC_CDP_CLIENT_API_KEY || "your-api-key"} 
-      chain={baseSepolia}
+      chain={celoAlfajores}
     >
       {children}
     </MiniKitProvider>
